0ab8d4292227a4f9e0c4339bf508cdf9e2149549
diyav0806 <142263702+diyav0806@users.noreply.github.com>
2024-09-04 23:58:38 +0530

    Update page.js

--- page.js
+++ page.js
@@ -1,12 +1,55 @@
+
+'use client';
 import Image from "next/image";
-import styles from "./page.module.css";
+import {useState, useEffect} from 'react';
+import {firestore} from '@/firebase';
+import { Box } from '@mui/material';
+import { query } from 'firebase/firestore';
 
 export default function Home() {
+  const [inventory, setInventory] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [itemName, setItemName] = useState('');
+
+  const updateInventory = async () => {
+    const snapshot = query(collection(firestore, 'inventory'));
+    const docs = await getDocs(snapshot);
+    const inventoryList = [];
+    docs.forEach((doc) => {
+      inventoryList.push({
+        name: doc.id,
+      ...doc.data(),
+    })
+
+      })
+    setInventory(inventoryList)
+      
+  }
+
+  const removeItem = async (item) =>{
+    const docRef = doc(collection(firestore, 'inventory'), item);
+    const docSnap = await getDoc(docRef); 
+  }
+
+  useEffect(() => {
+    updateInventory()
+  }, []
+)
+
+
   return (
-    <main>
-      <div>
-        <h1>Expense Tracker</h1>
-      </div>
-    </main>
-  );
+    <Box>
+      <Typography variant = "h1">Inventory Management</Typography>
+      {
+        inventory.forEach((item)=>{
+          return(
+          <>
+            {item.name}
+            {item.count}
+          </>)
+        })
+      }
+    </Box>
+  )
+
 }
